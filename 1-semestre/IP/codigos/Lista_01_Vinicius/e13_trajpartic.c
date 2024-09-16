9c045faf5a294c399022e5a657e2030f0e5453d3
muri-arte3d <muriloarte3d@gmail.com>
2024-09-16 11:54:50 -0300

    Lista atualiazada - Vinicius

---
+++ 1-semestre/IP/codigos/Lista_01_Vinicius/e13_trajpartic.c
@@ -0,0 +1,14 @@
+/*
+* Calculo de trajetoria de particula
+* Nome: Murilo Bueno Julião Lemos
+* Data: 15/09/2024
+*/
+
+#include <stdio.h>
+
+int main(){
+    int a, b, c; // coeficiente de pol de grau 2
+    
+
+    return 0;
+}
